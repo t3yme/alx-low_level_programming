f6a2aea019f35b637b2e3cd89cc7e45ddf48109b
t3yme <temitopebabatunde2024@gmail.com>
2023-08-13 08:17:49 -0700

    A program that prints all possible combinations of single-digit numbers followed by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - The entry point for the programme
+ * Return: Main evaluates to zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
