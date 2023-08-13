cf4550ea2d2424d066cf1212416171fd54578d98
t3yme <temitopebabatunde2024@gmail.com>
2023-08-13 07:09:59 -0700

    A function to print lowercase followed by uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - The entry point for the programme
+ * Return: Main evaluates to zero
+ */
+int main(void)
+{
+	char lowAlphabet;
+	char upperAlphabet;
+
+	for (lowAlphabet = 'a'; lowAlphabet <= 'z'; lowAlphabet++)
+		putchar(lowAlphabet);
+	for (upperAlphabet = 'A'; lowAlphabet <= 'Z'; upperAlphabet++)
+		putchar(upperAlphabet);
+	putchar('\n');
+	return (0);
+}
