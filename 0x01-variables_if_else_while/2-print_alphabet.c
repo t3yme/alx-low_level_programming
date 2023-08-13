d84699ea46fa484c42a6878bdcf4383d62f7de9c
t3yme <temitopebabatunde2024@gmail.com>
2023-08-13 06:55:39 -0700

    A function to print lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - The entry point for the programme
+ * Return: Main evaluates to zero
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
