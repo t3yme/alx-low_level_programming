0dcef83bbb37b43a372f531a372bb7676532fdd9
t3yme <temitopebabatunde2024@gmail.com>
2023-08-16 17:25:59 -0700

    A function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,13 @@
 /* more headers goes there */
 
 /**
- * _islower - Defines the function to determine a lower case letter
+ * _isalpha - Defines the function to check for alphabetic character.
  * @c: parameter provided for testing
  * Return: evaluates to integer on success
  */
-
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
 }
