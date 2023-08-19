647e78d19b1bea468584ac911e2355d0c2da0b4c
t3yme <temitopebabatunde2024@gmail.com>
2023-08-18 18:36:08 -0700

    a function that prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = '0'; i < '15'; i++)
+		for (i = 0; i < 15; i++)
 		{
-			if (i > '9')
-				_putchar('1');
-			_putchar(i);
+			if (i > 9)
+				_putchar(1 + '0');
+			_putchar((i % 10) + '0');
 		}
 	_putchar('\n');
 	}
