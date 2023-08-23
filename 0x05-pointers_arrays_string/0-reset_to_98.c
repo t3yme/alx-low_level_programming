20c4d9bcaa5b1e8a9d4f9be2856e951957391bba
t3yme <temitopebabatunde2024@gmail.com>
2023-08-22 18:08:07 -0700

    function to set pointer value to 98

---
+++ 0x05-pointers_arrays_string/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reset_to_98 - a function that resets a pointer value to 98
+ * @n: parameter provided for testing
+ * Return: evaluates to integer on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
