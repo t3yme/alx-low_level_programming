dc401e0e79af53fe9681439da359786bf52fa3f4
t3yme <temitopebabatunde2024@gmail.com>
2024-01-11 09:24:08 +0000

    A function that prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void _print_rev_recursion - Prints string in reverse
+ * _print_rev_recursion - Prints string in reverse
  * @s: Given string
  */
 
@@ -9,7 +9,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 	}
 	else
 	{
